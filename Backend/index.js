f2abea814dcb59c10b4a4740eb999f589fa97211
Naman Mishra <125729522+Naman-Mishra-3116@users.noreply.github.com>
2024-08-26 16:01:36 +0530

    updated index.json

--- Backend/index.js
+++ Backend/index.js
@@ -22,7 +22,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors(test));
 
-app.get("/test", (_, res) => {
+app.get("/", (_, res) => {
   res.send("Hello world");
 });
 
